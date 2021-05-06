ca85227a98828b236a89c90b9911261224f0d18f
lily9494 <ahmadi.nilufar12@gmail.com>
2021-05-06 23:00:14 +0200

    merge form main

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const port =8080;
-http=require("http");
-express=require("express");
+const port = 8080;
+http = require("http");
+express = require("express");
 layouts = require("express-ejs-layouts");
 const homeController = require("./controllers/homeController");
+const errorController = require("./controllers/errorController");
 
-app=express();
-app.use(layouts)
+app = express();
+app.use(express.static
+  ("public"));
+app.use(layouts);
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.json());
@@ -16,7 +19,7 @@ app.use(express.urlencoded({
     app.get("/profile",  homeController.respondWithName);
     app.get("/",  homeController.respondInfo);
 app.get("/home/:userHome", homeController.sendReqParam)
-   .listen(port, () => {
+  .listen(port, () => {
     console.log(`The Express.js server has started and is listening
    ➥ on port number: ${port}`);
    });
@@ -46,7 +49,7 @@ app.get("/home/:userHome", homeController.sendReqParam)
 //     res.writeHead(httpStatus.OK,contentTypes.html);
 //     utiles.getFile("views/home.html",res);
 //     });
-  
+
 
 // http.createServer(router.handle).listen(port);
 // console.log("works")
