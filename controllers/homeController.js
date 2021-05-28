eb68651793a95ea76a1cbaae7e3caf06e9bda208
bulaos <59624661+bulaos@users.noreply.github.com>
2021-05-28 23:53:21 +0200

    add and update artPiece model, add view for artpieces

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,26 +1,24 @@
 exports.sendReqParam = (req, res) => {
-    let userHome = req.params.userHome;
-    res.render("home", { name: userHome });
-    // res.send(`This is the homepage for ${userHome}`);
-    
-   };
-   exports.respondWithName = (req, res) => {
-    let paramsName = req.params.myName;
- res.render("profile", { name: paramsName });
-   }
-   exports.respondHomePageWebsite = (req, res) => {
-    
- res.render("homePageWebsite");
-   }
-   exports.respondProfile = (req, res) => {
-    
-    res.render("profile");
-      }
-   exports.registration = (req, res) => {
-    
-    res.render("register");
-      } 
-   exports.respondWithLogin = (req, res) => {
-    
-    res.render("login");
-      } 
+  let userHome = req.params.userHome;
+  res.render("home", { name: userHome });
+  // res.send(`This is the homepage for ${userHome}`);
+};
+exports.respondWithName = (req, res) => {
+  let paramsName = req.params.myName;
+  res.render("profile", { name: paramsName });
+};
+exports.respondHomePageWebsite = (req, res) => {
+  res.render("homePageWebsite");
+};
+exports.respondProfile = (req, res) => {
+  res.render("profile");
+};
+exports.registration = (req, res) => {
+  res.render("register");
+};
+exports.respondWithLogin = (req, res) => {
+  res.render("login");
+};
+exports.addNewArtPiece = (req, res) => {
+  res.render("addNewArtPiece");
+};
